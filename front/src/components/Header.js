e7e1f1b1ab3c573f49c26e16ef532ccf47446f00
Rain <hramil0120@gmail.com>
2020-12-04 18:14:29 +0300

    Add the lab's variant to Header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { Flex, Center, Container, Spacer, Box, Image, Text } from "@chakra-ui/react";
+import styled from '@emotion/styled';
 import logo from "../pictures/logo_light.png";
 
-const Logo = () => (
-  <Box>
-    <Flex alignItems="center">
-      <Image src={logo} alt="logo" boxSize="9rem" objectFit="contain" />
-      <Text as="span" ml="1">Лабораторная 4</Text>
-    </Flex>
-  </Box>
-);
 
 const LabInfoValue = ({ children }) => <Text as="span" color="cyan.100" fontWeight="bold">{children}</Text>;
 
@@ -17,6 +10,18 @@ const LabInfo = () => (
   <Box fontSize="0.75em">
     <Box>Группа: <LabInfoValue>P3212</LabInfoValue></Box>
     <Box>Сделалъ: <LabInfoValue>Хузин Рамиль</LabInfoValue></Box>
+  </Box>
+);
+
+const Logo = () => (
+  <Box>
+    <Flex alignItems="center">
+      <Image src={logo} alt="logo" boxSize="9.5rem" objectFit="contain" />
+      <Box ml="1">
+        <Text lineHeight="short">Лабораторная 4</Text>
+        <Text fontSize={{ base: "0.75em", md: "0.65em" }} lineHeight="none" position="absolute">Вариант: <LabInfoValue>31772</LabInfoValue></Text>
+      </Box>
+    </Flex>
   </Box>
 );
 
@@ -30,7 +35,7 @@ export const Header = () => (
     boxShadow="lg"
   >
     <Container maxW="lg">
-      <Flex height="4.5rem" alignItems="center">
+      <Flex height="5rem" alignItems="center">
         <Logo />
         <Spacer />
         <LabInfo />
